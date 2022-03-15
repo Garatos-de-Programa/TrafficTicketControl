b9b5a4679e7a003d1f5ce5d9e97720954b9d11e9
jordaogustavo <jordaogustavo3@gmail.com>
2022-03-14 21:44:54 -0300

    :rocket: add traffic violation and user mongo context

---
+++ src/TrafficTicket/TrafficTicket.Api/Data/TrafficViolation/TrafficViolationContext.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using TrafficTicket.Api.Models;
+
+namespace TrafficTicket.Api.Data
+{
+    public class TrafficViolationContext : ITrafficViolationContext
+    {
+        public IMongoCollection<TrafficViolation> TrafficViolations { get; }
+
+        public TrafficViolationContext(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+            var databaseName = configuration.GetValue<string>("DatabaseSettings:DatabaseName");
+            var collectionName = configuration.GetValue<string>("DatabaseSettings:CollectionName");
+
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(databaseName);
+
+            TrafficViolations = database.GetCollection<TrafficViolation>(collectionName);
+        }
+
+    }
+}
